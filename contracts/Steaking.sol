367f8359a093007665e6d372cbcbad997e477fe3
cambyses21 <adcv@hey.com>
2022-08-09 20:46:24 -0700

    Final version ahead of submit

--- contracts/Steaking.sol
+++ contracts/Steaking.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 
 // Contract to handle staking and to hold the key on the rewards to be distributed
 // One layer of staking rewards distributed to senior time-locked the other to a junior tranche
-// Reward balances are updated with every interaction
+// Reward balance are updated with every interaction
 // Edge case to test for is a situation where there are no further interactions so the maths has to handle how to share between 1 senior and 1 junior token
 
 import "./IERC20.sol";
@@ -16,9 +16,16 @@ contract Staking {
 
     address public owner;
 
-    mapping(address => Steaks) public balance;
+    mapping(address => uint256) public balance; // Original staked amount per address
+    mapping(address => uint256) public rewards; // Rewards accumulated per address
+    mapping(address => uint256) public rewardsPTA; // Rewards per token per address for the previous period
 
-    uint256 public totalRewards = 1000; // Hardcoded here but should be eventually pointing at an address for a dynamic reward rate
+    uint256 public rewardBalance = 1000; // Hardcoded here but should be eventually pointing at an address for a dynamic reward rate
+
+    uint256 public rewardRate; // Rate per second for accumulating rewards
+    uint256 public totalSupply; // Total sum staked
+    uint256 public time; // Timestamp for counting the staking periods
+    uint256 public rewardPT; // Helper to essentially allocate the rewards in a given time period to a balance
 
     // The contract is deployed by an owner who gates the most important function
     constructor(address _stakingToken, address _receiptToken) {
@@ -32,7 +39,90 @@ contract Staking {
         _;
     }
 
-    //modifier updateRewards() {}
+    modifier updateRewards(address account_) {
+        rewardPT = updateRewardsPerToken();
+        time = block.timestamp;
+
+        if (account_ != address(0)) {
+            rewards[account_] = earned(account_); // Having earned as a public view function can provide a hook to query accumulated rewards without drawing gas
+            rewardsPTA[account_] = rewardPT;
+        }
+
+        _;
+    }
+
+    // Algorithm
+    // Basis from Solidity by Example
+    //
+
+    // Please express as per second rate by taking the 31536000th root of an annualized rate
+    function setRate(uint256 annualRate) external onlyOwner {
+        rewardRate = annualRate;
+    }
+
+    // Start the clock with stake:
+    function stake(uint256 amount_) public {
+        // To include an update rewards modifier
+        require(amount_ > 0, "Amount must be >0");
+        stakingToken.transferFrom(msg.sender, address(this), amount_);
+        receiptToken.transferFrom(address(0), msg.sender, amount_);
+        balance[msg.sender] += amount_;
+        totalSupply += amount_;
+    }
+
+    function getRewards() public updateRewards(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            stakingToken.transfer(msg.sender, reward);
+        }
+    }
+
+    function updateRewardsPerToken() public view returns (uint256) {
+        if (totalSupply == 0) {
+            return rewardPT;
+        }
+
+        // Catching the case when there is no more reward balance left to distribute
+        if ((rewardRate * (block.timestamp - time) * 1e18) > rewardBalance) {
+            return
+                rewardPT +
+                ((rewardRate * (block.timestamp - time) * 1e18) -
+                    rewardBalance) /
+                totalSupply;
+        }
+
+        return
+            rewardPT +
+            (rewardRate * (block.timestamp - time) * 1e18) /
+            totalSupply;
+    }
+
+    function earned(address account_) public returns (uint256) {
+        // Forcing rewardBalance to never go below 0 - it is used immediately after in updateRewardsPerToken()
+        rewardBalance = _max(
+            0,
+            _min(
+                rewardBalance,
+                ((balance[account_] *
+                    (updateRewardsPerToken() - rewardsPTA[account_])) * 1e18) +
+                    rewards[account_]
+            )
+        );
+
+        return
+            ((balance[account_] *
+                (updateRewardsPerToken() - rewardsPTA[account_])) * 1e18) +
+            rewards[account_];
+    }
+
+    function withdraw(uint256 amount_) public {
+        require(amount_ > 0, "Amount must be >0");
+        amount_ = _min(balance[msg.sender], amount_);
+        balance[msg.sender] -= amount_;
+        totalSupply -= amount_;
+        stakingToken.transfer(msg.sender, amount_);
+    }
 
     // Helper Functions
 
@@ -41,5 +131,7 @@ contract Staking {
         return x <= y ? x : y;
     }
 
-    // Calculate the time rewards were last calculated
+    function _max(uint256 x, uint256 y) private pure returns (uint256) {
+        return x >= y ? x : y;
+    }
 }
--- contracts/Steaks.sol
+++ contracts/Steaks.sol
@@ -2,71 +2,168 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
-// Contract to handle staking and to hold the key on the rewards to be distributed
-// One layer of staking rewards distributed to senior time-locked the other to a junior tranche
-// Reward balances are updated with every interaction
-// Edge case to test for is a situation where there are no further interactions so the maths has to handle how to share between 1 senior and 1 junior token
+// Steaks will replicate the main features of an ERC20 token for the token and the receipt
+// Implementing both on the same contract for clarity though I suppose you should probably separate them
+// Also imagine that just importing the ERC20 contract spec from OZ is better practice but I am writing this to learn and figure out what all the moving parts are
+// So Steaks will have to implement all of the ERC20 token spec all over again
+
+// Heavily inspired by ERC20 but trying to implement it through paraphrasing the key functions this token will need
 
 import "./IERC20.sol";
-import "./Steaks.sol";
 
-contract Staking {
-    Steaks public stakingToken;
-    SteakReceipts public receiptToken;
+contract Steaks is IERC20 {
+    // Attributes and events
 
-    address public owner;
+    // Following ERC20 pattern, two events on function calls that document transfers and approvals
+    // Adding a third event to document a Staking event
+    event Staked(address sender, uint256 value);
+    event Unstaked(address recipient, uint256 value);
 
-    mapping(address => uint256) public balance; // Original staked amount per address
-    mapping(address => uint256) public rewards; // Rewards accumulated per address
+    // _balances is a key value pair that stores the amount of tokens against each address
+    // _allowances is a security function that explicitly asks for approval to change balances on behalf of other addresses
+    mapping(address => uint256) public override balanceOf;
+    mapping(address => mapping(address => uint256)) public override allowance;
 
-    uint256 public maxRewards = 1000; // Hardcoded here but should be eventually pointing at an address for a dynamic reward rate
-    uint256 public rewardRate; // Rate per second for accumulating rewards
-    uint256 public rewardSum; // Sum of rewardRate * dt * 1e18 / totalSupply - formula from solidity by example
-    uint256 public totalSupply;
+    // The Steak token will have a fixed token supply. The Steak Receipt token will have a malleable token supply
+    uint256 public override totalSupply = 1000;
+    uint256 public maxSupply = 2000;
+    string public name = "Steak Tokens";
+    string public symbol = "STKS";
+    uint256 public decimals = 18;
 
-    // The contract is deployed by an owner who gates the most important function
-    constructor(address _stakingToken, address _receiptToken) {
-        owner = msg.sender;
-        stakingToken = Steaks(_stakingToken);
-        receiptToken = SteakReceipts(_receiptToken);
+    // Require transfer function to work only if toggle is true
+
+    function transfer(address to, uint256 value)
+        external
+        override
+        returns (bool)
+    {
+        require(balanceOf[msg.sender] >= value, "Not enough balance");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only owner address can set");
-        _;
+    function approve(address spender, uint256 amount)
+        external
+        override
+        returns (bool)
+    {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
 
-    function stake(uint256 amount_) public {
-        // To include an update rewards modifier
-        require(amount_ > 0, "Amount must be >0");
-        stakingToken.transferFrom(msg.sender, address(this), amount_);
-        receiptToken.transferFrom(address(0), msg.sender, amount_);
-        balance[msg.sender] += amount_;
-        totalSupply += amount_;
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external override returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
     }
 
-    function getRewards() public {
-        uint256 reward = rewards[msg.sender];
-        if (reward > 0) {
-            rewards[msg.sender] = 0;
-            stakingToken.transferFrom(address(0), msg.sender, amount_);
-        }
+    function mint(uint256 amount) external returns (bool) {
+        require(
+            totalSupply + amount <= maxSupply,
+            "Cannot mint more than total supply of Steaks"
+        );
+
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+        return true;
     }
 
-    function withdraw(uint256 amount_) public {
-        require(amount_ > 0, "Amount must be >0");
-        getRewards();
-        balances[msg.sender] -= amount_;
-        totalSupply -= amount;
-        stakingToken.transfer(msg.sender, amount_);
+    function burn(uint256 amount) external returns (bool) {
+        require(
+            totalSupply - amount >= 0,
+            "Cannot burn more than the total supply"
+        );
+
+        balanceOf[msg.sender] -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+        return true;
+    }
+}
+
+contract SteakReceipts is IERC20 {
+    // Attributes and events
+
+    // Following ERC20 pattern, two events on function calls that document transfers and approvals
+    // Adding a third event to document a Staking event
+    event Staked(address sender, uint256 value);
+    event Unstaked(address recipient, uint256 value);
+
+    // _balances is a key value pair that stores the amount of tokens against each address
+    // _allowances is a security function that explicitly asks for approval to change balances on behalf of other addresses
+    mapping(address => uint256) public override balanceOf;
+    mapping(address => mapping(address => uint256)) public override allowance;
+
+    // The Steak token will have a fixed token supply. The Steak Receipt token will have a malleable token supply
+    uint256 public override totalSupply = 0;
+
+    string public name = "Steak Receipts";
+    string public symbol = "stSTKS";
+    uint256 public decimals = 18;
+
+    // Require transfer function to work only if toggle is true
+
+    function transfer(address to, uint256 value)
+        external
+        override
+        returns (bool)
+    {
+        require(balanceOf[msg.sender] >= value, "Not enough balance");
+
+        balanceOf[msg.sender] -= value;
+        balanceOf[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
     }
 
-    // Helper Functions
-
-    // Solidity doesn't have max/min functions
-    function _min(uint256 x, uint256 y) private pure returns (uint256) {
-        return x <= y ? x : y;
+    function approve(address spender, uint256 amount)
+        external
+        override
+        returns (bool)
+    {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
     }
 
-    // Calculate the time rewards were last calculated
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external override returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) external returns (bool) {
+        require(
+            totalSupply - amount >= 0,
+            "Cannot burn more than the total supply"
+        );
+
+        balanceOf[msg.sender] -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+        return true;
+    }
 }
